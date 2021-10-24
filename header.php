9a244cb24c8fc20f2b991faedc39126c767b3785
Uzaiz Sumit <62895440+Uz17@users.noreply.github.com>
2021-10-25 00:43:39 +0600

    commit

---
+++ header.php
@@ -0,0 +1,43 @@
+ <!DOCTYPE html>
+ <html lang="">
+
+ <head>
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>Bangladesh Police Dashboard and Criminal Record </title>
+
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+   <link rel="stylesheet" type="text/css" href="assets/css/bootstrap-theme.min.css">
+
+   <!-- Custom CSS -->
+   <link href="assets/css/simple-sidebar.css" rel="stylesheet">
+   <link href="assets/css/dataTables.bootstrap.min.css" rel="stylesheet">/
+
+
+ </head>
+
+ <body>
+
+   <div class="container " style="margin-bottom: 70px;margin-top: 50px;">
+     <div class="row " style="
+
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+    
+     ">
+
+       <div>
+         <img src="logo.png" style=" width: 75%;" class="">
+       </div>
+
+       <div class="" style="
+          margin-left: 45px;
+          font-size: 31px;
+       ">
+         Bangladesh Police Dashboard and Criminal Record Management
+       </div>
+     </div>
+   </div>
